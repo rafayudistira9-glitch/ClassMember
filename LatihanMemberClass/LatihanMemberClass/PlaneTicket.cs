36543aed73c0a4ad3f832f7253218b9e608d3f22
rafayudistira9-glitch <rafayudistira9@gmail.com>
2026-04-21 14:56:49 +0700

    Add files via upload

---
+++ LatihanMemberClass/LatihanMemberClass/PlaneTicket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class PlaneTicket
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int Cost { get; set; }
+        public string Currency { get; set; }
+
+    }
+}
