f4c322c2ca63a366395ba120f9b0789944e5769f
rafayudistira9-glitch <rafayudistira9@gmail.com>
2026-04-24 00:03:00 +0700

    Update PlaneTicket.cs

--- LatihanMemberClass/LatihanMemberClass/PlaneTicket.cs
+++ LatihanMemberClass/LatihanMemberClass/PlaneTicket.cs
@@ -4,14 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LatihanMemberClass
+namespace PlaneTicket
 {
     internal class PlaneTicket
     {
-        public string Origin { get; set; }
-        public string Destination { get; set; }
-        public int Cost { get; set; }
-        public string Currency { get; set; }
+        private string _origin;
+        private string _dest;
+        private int _cost;
+        private string _con;
+
+        public string Origin
+        {
+            get // get accessor
+            {
+                return _origin;
+            }
+            set // set accessor
+            {
+                _origin = value;
+            }
+        }
+
+        public string Destination
+        {
+            get // get accessor
+            {
+                return _dest;
+            }
+            set // set accessor
+            {
+                _dest = value;
+            }
+        }
+
+        public int Cost
+        {
+            get // get accessor
+            {
+                return _cost;
+            }
+            set // set accessor
+            {
+                _cost = value;
+            }
+        }
+
+        public string Currency
+        {
+            get // get accessor
+            {
+                return _con;
+            }
+            set // set accessor
+            {
+                _con = value;
+            }
+        }
 
     }
 }
